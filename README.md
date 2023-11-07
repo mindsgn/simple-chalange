e498d04c5fa337254bff70595bd0df42351a6ec9
mindsgn <seni.tembe@protonmail.com>
2023-11-07 13:27:42 +0200

    update

---
+++ README.md
@@ -0,0 +1,9 @@
+# Even Number Sum
+
+A function that takes in an array of integers and returns the sum of all even numbers in the array.
+
+### How to run
+
+```
+    node sum.js
+```
--- sum.js
+++ sum.js
@@ -8,6 +8,6 @@ function sumOfEvenNumbers(arr) {
   return sum;
 }
 
-const numbers = [1, 2, 3, 4, 5, 6, 9, 10, 11, 29, 50, 87, 11];
+const numbers = [5, 7, 2, 6, 8, 9, 1, 10, 11, 29, 50, 87, 11];
 const result = sumOfEvenNumbers(numbers);
 console.log(result);
